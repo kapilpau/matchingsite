873a111ab71d8959925a160f3e082a93568dc900
Kapil <k.d.n.pau@se15.qmul.ac.uk>
2018-11-13 12:53:10 +0000

    Admin can add and delete hobbies

--- matchingapp/urls.py
+++ matchingapp/urls.py
@@ -6,6 +6,10 @@ urlpatterns = [
     # main page
     path('', views.index, name='index'),
     path('profile/', views.profile, name='profile'),
-    path('administrator/', views.index, name='administrator'),
+    path('administrator/', views.admin, name='administrator'),
+    path('administrator/hobbies/', views.getHobbies, name='hobbyList'),
+    path('administrator/addHobby/', views.addHobby, name='addHobby'),
+    path('administrator/deleteHobby/', views.deleteHobby, name='deleteHobby'),
     path('login/', views.login, name='login'),
+    path('signup/', views.signup, name='signup'),
 ]
