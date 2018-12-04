0b1d1dfe6818d4a1b8c2dd81f9271b3f0ca3c9b3
Kapil <k.d.n.pau@se15.qmul.ac.uk>
2018-12-04 22:27:29 +0000

    Redirect to home if the user tries to access conversation they are not in

--- matchingapp/views.py
+++ matchingapp/views.py
@@ -371,6 +371,8 @@ def conversation(request, id):
     try:
         msgs = Message.objects.order_by('sent_at').filter(conversation=Conversation.objects.get(id=id))
         user = Member.objects.get(username=request.session['username'])
+        if user not in Conversation.objects.get(id=id).participants.all():
+            redirect('/')
         contextMsgs = []
         for msg in msgs:
             contextMsgs.append({'sender': msg.sender.profile.name, 'sent_at': msg.sent_at.strftime("%Y-%m-%d %H:%M:%S"), 'contents': msg.contents})
